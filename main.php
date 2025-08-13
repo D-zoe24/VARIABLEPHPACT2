ee67e5c27b664ea583e3e986c268b6367430d010
D-zoe24 <devarasnikki6@gmail.com>
2025-08-13 02:18:10 +0000

    UnderstandingTheVariables

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Nikki Devaras";
+    $age = 20;
+    $school = "Global Reciprocal Colleges";
+    $course = "Bachelor of Science in Information Technology";
+    $subject = "WEBSYS";
+    $student = true;
+
+    echo "Name: $name \n";
+    echo "Age: $age\n";
+    echo "School: $school\n";
+    echo "Course: $course\n";
+    echo "Subject: $subject\n";
+    echo "Student: ". ($student ? "Yes" : "NO") . "\n";
+  ?>
